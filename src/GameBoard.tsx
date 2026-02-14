7c074415582ccbe054604409ad6dd59f5c39decc
Daniel Alexander Nedergaard <danieldownfarm@gmail.com>
2026-02-14 16:29:40 +0100

    Attempted to add a second player, doesn't work yet

--- src/GameBoard.tsx
+++ src/GameBoard.tsx
@@ -1,16 +1,25 @@
 import { useState, useRef } from "react"
 
 export default function GameBoard() {
-    const [gameMessage, setGameMessage] = useState<string>("Player X's turn");
     const [tiles, setTiles] = useState<(string | null)[]>(Array(9).fill(null));
-    const tileIndex =useRef<number | null>(null);
+    const currentPlayer =useRef<string>("X");
+    const [gameMessage, setGameMessage] = useState<string>(`Player ${currentPlayer.current}'s turn`);
 
     function tileClicked(index: number) {
         setTiles(previousArray => {
             const newArray = [...previousArray];
-            newArray[index] = "X";
+            newArray[index] = currentPlayer.current;
             return newArray;
         });
+
+        // Switch player
+        if (currentPlayer.current == "X") {
+            currentPlayer.current = "O";
+            setGameMessage(`Player ${currentPlayer.current}'s turn`);
+        } else {
+            currentPlayer.current = "X";
+            setGameMessage(`Player ${currentPlayer.current}'s turn`);
+        }
     }
 
     return (
