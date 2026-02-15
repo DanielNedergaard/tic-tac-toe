b3c65c70c6b7d8d74e47ea89ef740740b86a1711
Daniel Alexander Nedergaard <danieldownfarm@gmail.com>
2026-02-15 13:27:17 +0100

    Removed currently unused import

--- src/GameBoard.tsx
+++ src/GameBoard.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from "react"
+import { useState, useEffect } from "react"
 
 export default function GameBoard() {
     const [tiles, setTiles] = useState<(string | null)[]>(Array(9).fill(null));
